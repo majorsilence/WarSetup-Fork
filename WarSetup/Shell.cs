147ef6a61d75078074dc2f4d0a3d8b0a97c2926b
Peter Gill <peter.gill@townsuite.com>
2013-12-12 11:54:35 -0800

    Auto detect wix 3.7 and 3.8.

--- WarSetup/Shell.cs
+++ WarSetup/Shell.cs
@@ -98,8 +98,18 @@ namespace WarSetup
                         rval = System.Environment.GetEnvironmentVariable("ProgramFiles");
                     if ((rval == null) || (rval == ""))
                         rval = @"C:\Program Files";
-
-                    if (System.IO.Directory.Exists(System.IO.Path.Combine(rval, "WiX Toolset v3.6")))
+                    
+                    if (System.IO.Directory.Exists(System.IO.Path.Combine(rval, "WiX Toolset v3.8")))
+                    {
+                        // Attempt to use version 3.6 new directory
+                        rval = System.IO.Path.Combine(rval, "WiX Toolset v3.8");
+                    }
+                    else if (System.IO.Directory.Exists(System.IO.Path.Combine(rval, "WiX Toolset v3.7")))
+                    {
+                        // Attempt to use version 3.6 new directory
+                        rval = System.IO.Path.Combine(rval, "WiX Toolset v3.7");
+                    }
+                    else if (System.IO.Directory.Exists(System.IO.Path.Combine(rval, "WiX Toolset v3.6")))
                     {
                         // Attempt to use version 3.6 new directory
                         rval = System.IO.Path.Combine(rval, "WiX Toolset v3.6");
